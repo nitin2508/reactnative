43fd2c297553f8286b8f02aaa4cd55d7ac1c152d
Gaurav Nagar <grvngr@gmail.com>
2018-04-10 22:35:15 +0530

    93

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,22 +1,62 @@
 import React,{Component} from 'react';
-import {View ,Text , Button} from 'react-native';
+import {View ,Text , Button,TextInput,StyleSheet,ImageBackground} from 'react-native';
 import startMainTabs from '../MainTabs/startMainTab'; 
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
+import  ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 
 class AuthScreen extends Component{
-
     loginHandler(){
         startMainTabs();
     }
-
-    render(){
+    render(){ 
         return(
-            <View>
-                <Text>Auth Screen</Text>
-                 <Button title="login" onPress={this.loginHandler}/> 
+             <ImageBackground source={backgroundImage} style={styles.backgroundImage} >
+            <View style={styles.container}>
+               
+                    <MainText>
+                         <Text style={styles.textHeading}>Please Login</Text>
+                    </MainText>
+                     <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler} >Switch to login</ButtonWithBackground>  
+                        <View style={styles.inputContainer}>
+                            <DefaultInput style={styles.input}  placeholder="Your E-Mail Address"/>
+                            <DefaultInput style={styles.input}  placeholder="Password"/>
+                            <DefaultInput style={styles.input}  placeholder="Confirm Password"/> 
+                        </View>  
+                     <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler} >Submit</ButtonWithBackground> 
             </View>
-        );
+             </ImageBackground>
+         );
     }
 }
+
+const styles= StyleSheet.create({
+    container:{
+        borderColor:"red",
+        borderWidth:1,
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    inputContainer:{
+        width:"80%"
+    },
+    backgroundImage:{
+        width:'100%',
+        flex:1
+    },
+    input:{
+        backgroundColor:"#eee",
+        borderColor:"#bbb"
+    },
+    textHeading:{
+        fontSize:28,
+        fontWeight:"bold"
+    }
+  
+});
 
 
 export default AuthScreen;
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,8 +1,12 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import {View,Text} from 'react-native';
+import {View,Text,Button,TextInput,StyleSheet,ScrollView,Image} from 'react-native';
 import PlaceInput from '../../components/PlaceInput/PlaceInput'
 import {addPlace} from '../../store/actions/index';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
+import imagePlaceholder from '../../assets/beautiful-place.jpg';
 class SharePlaceScreen extends Component{
     constructor(props){
         super(props);
@@ -25,12 +29,44 @@ class SharePlaceScreen extends Component{
     }
     render(){
         return(
-            <View>
+            <ScrollView>
+                <View style={styles.container}>
+                <MainText><HeadingText>Share a Place with us</HeadingText></MainText>
+                <View style={styles.placeholder}><Image style={styles.previewImage} source={imagePlaceholder}/></View>
+                <View style={styles.button}>
+                <Button onPress={this.placeAddedHandler}  title="Pick Image"></Button>
+                </View>
+                <View style={styles.placeholder}><Text>Map</Text></View>
+                <Button onPress={this.placeAddedHandler} title="Locate Me"></Button>
+                <DefaultInput placeholder="Placename"/>
+                <Button onPress={this.placeAddedHandler}  title="Share the place"></Button>
                 <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-            </View>
+                </View>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center"
+    }, 
+    placeholder:{
+        borderWidth:1,
+        borderColor:"black",
+        backgroundColor:'#eee',
+        width:"80%",
+        height:150
+    },
+    button:{
+        margin:8
+    },
+    previewImage:{
+        width:"100%",
+        height:"100%"
+    }
+})
 
 const mapDispatchToProps = dispatch =>{
     return{
